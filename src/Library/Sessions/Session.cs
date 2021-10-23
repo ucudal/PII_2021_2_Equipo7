564367006d3422b970f5b2c2043e401836bb0280
Rodrigo Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-23 14:24:24 -0300

    Añadidas clases de manejo de sesiones.

--- src/Library/Sessions/Session.cs
+++ src/Library/Sessions/Session.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Library
 {
+    /// <summary>
+    /// Clase que contiene los datos de una sesion de usuario.
+    /// </summary>
     public class Session
     {
         private MessagingService service;
@@ -11,6 +14,21 @@ namespace Library
         private DProcessData process;
         private DateTime lastActivity;
 
+        /// <summary>
+        /// Crea una instancia de Session a partir de una
+        /// cuenta en un servicio de mensajeria y la posicion
+        /// En el arbol de discusiones con el Bot.
+        /// </summary>
+        /// <param name="service">
+        /// Servicio de mensajeria.
+        /// </param>
+        /// <param name="account">
+        /// Identificador del usuario en el servicio de mensajeria.
+        /// </param>
+        /// <param name="menuLocation">
+        /// Posicion actual del usuario en el arbol de discusiones
+        /// con el Bot.
+        /// </param>
         public Session(MessagingService service, string account, string menuLocation)
         {
             this.service = service;
@@ -19,8 +37,22 @@ namespace Library
             this.lastActivity = DateTime.Now;
         }
 
+        /// <summary>
+        /// Servicio de mensajeria.
+        /// </summary>
+        /// <value><c>MessagingService</c></value>
         public MessagingService Service { get => this.service; }
+        
+        /// <summary>
+        /// Identificador en el servicio de mensajeria.
+        /// </summary>
+        /// <value><c>string</c></value>
         public string Account { get => this.account; }
+
+        /// <summary>
+        /// Localizacion del usuario en el arbol de discusiones.
+        /// </summary>
+        /// <value><c>string</c></value>
         public string MenuLocation
         {
             get => this.menuLocation;
@@ -30,11 +62,21 @@ namespace Library
                 this.lastActivity = DateTime.Now;
             }
         }
+
+        /// <summary>
+        /// Ultimo momento de actividad del usuario.
+        /// </summary>
+        /// <value><c>DateTime</c></value>
         public DateTime LastActivity
         {
             get => this.lastActivity;
             set => this.lastActivity = value;
         }
+
+        /// <summary>
+        /// Datos del proceso actual si hay uno activo.
+        /// </summary>
+        /// <value><c>DProcessData</c></value>
         public DProcessData Process
         {
             get => this.process;
@@ -45,6 +87,10 @@ namespace Library
             }
         }
 
+        /// <summary>
+        /// Id del usuario en la plataforma
+        /// </summary>
+        /// <value><c>int</c></value>
         public int UserId 
         { 
             get => this.userId;
