3429bc89ae2afa8daedfe36817ea3cf3b6522605
Rodrigo Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-23 12:23:56 -0300

    Añadidos objetos de uso comun requeridos en el momento.

---
+++ src/Library/Common/Enums/MessagingService.cs
@@ -0,0 +1,14 @@
+namespace Library
+{
+    /// <summary>
+    /// Enumerable que lista los distintos
+    /// servicios de mensajeria soportados
+    /// por el Bot.
+    /// </summary>
+    public enum MessagingService
+    {
+        ///<summary>Canal de comunicacion por la consola</summary>
+        Console
+    }
+    
+}
---
+++ src/Library/Common/Singleton.cs
@@ -0,0 +1,35 @@
+namespace Library
+{
+    /// <summary>
+    /// Clase que permite reutilizar la misma instancia
+    /// de otra clase por todo el programa.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Clase de la cual se precisa utilizar la misma 
+    /// instancia.
+    /// </typeparam>
+    public class Singleton<T> where T : new()
+    {
+        private static T instance;
+
+        /// <summary>
+        /// Instancia compartida de la clase <c>T</c>.
+        /// </summary>
+        /// <value>
+        /// Singleton/Instancia de la clase <c>T</c>.
+        /// </value>
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+
+    }
+}
