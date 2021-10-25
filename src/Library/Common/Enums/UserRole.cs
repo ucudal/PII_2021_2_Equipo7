44ba148b63f37a7be68d087625b79b03202d059c
Rodrigo Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-24 21:24:00 -0300

    Enumerable con los roles de usuario agregado.

---
+++ src/Library/Common/Enums/UserRole.cs
@@ -0,0 +1,17 @@
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Enumerable que lista los roles
+    /// a los que pueden pertenecer los
+    /// usuarios.
+    /// </summary>
+    public enum UserRole
+    {
+        ///<summary>Administrador del sistema.</summary>
+        SystemAdministrator,
+        ///<summary>Administrador de empresa</summary>
+        CompanyAdministrator,
+        ///<summary>Emprendedor.</summary>
+        Entrepreneur
+    }
+}
