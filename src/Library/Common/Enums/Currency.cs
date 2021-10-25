6ce7aee3060674118086e25f020144b36fac0726
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-25 08:25:53 -0300

    Agregado enumerable con monedas.

---
+++ src/Library/Common/Enums/Currency.cs
@@ -0,0 +1,14 @@
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Enumerable que lista las monedas
+    /// manejadas por la plataforma.
+    /// </summary>
+    public enum Currency
+    {
+        ///<summary>Peso Uruguayo.</summary>
+        PesoUruguayo,
+        ///<summary>Dolar Estadounidense.</summary>
+        DolarEstadounidense
+    }
+}
