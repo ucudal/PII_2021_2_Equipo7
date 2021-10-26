8dea3767a6adae56691bd2dbc78ef29f434d948e
Nicolas Maisonnave <nicolas.maisonnave@correo.ucu.edu.uy>
2021-10-26 11:14:45 -0300

    Metodo getByCode

--- src/Library/InvitationAdmin.cs
+++ src/Library/InvitationAdmin.cs
@@ -4,9 +4,18 @@ using System.Collections.Generic;
 namespace ClassLibrary
 {
     /// <summary>
-     /// Esta clase representa la administracion de Invitaciones.
+    /// Esta clase representa la administracion de Invitaciones.
     /// </summary>
     public class InvitationAdmin: DataAdmin<Invitation>
     {
+        /// <summary>
+        /// Obetener la invitacion a partir de su codigo
+        /// </summary>
+        /// <param name="pCode">Codigo de la invtacion</param>
+        /// <returns></returns>
+        public Invitation GetByCode(string pCode)
+        {
+            return this.Items.Find(obj=>obj.Code==pCode);
+        }
     }
 }
