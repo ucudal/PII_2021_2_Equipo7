cb6da1bc8e5e8046acf3a1e4a0444f6f46cc8d90
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-28 10:36:32 -0300

    Añadido parametro de tipo a la interface de datos administables y metodos de serializacion.

--- src/Library/Common/Types/IManagableData.cs
+++ src/Library/Common/Types/IManagableData.cs
@@ -5,16 +5,29 @@ namespace ClassLibrary
     /// deben ser administradas por una implementacion
     /// concreta de <see cref="DataAdmin{T}"/>. 
     /// </summary>
-    public interface IManagableData
+    public interface IManagableData<T>
     {
         /// <summary>
         /// Identificador del registro de la estructura.
         /// </summary>
         int Id { get; set; }
-        
+
         /// <summary>
         /// Flag de la baja logica del registro.
         /// </summary>
         bool Deleted { get; set; }
+
+        /// <summary>
+        /// Cargar las propiedades del objeto desde un Json.
+        /// </summary>
+        /// <param name="json">Cadena json con los datos del objeto.</param>
+        void LoadFromJson(string json);
+
+        /// <summary>
+        /// Devuelve un nuevo objeto equivalente pero no identico
+        /// al cual se le envia el mensaje.
+        /// </summary>
+        /// <returns>Copia por miembro de un objeto.</returns>
+        T Clone();
     }
 }
