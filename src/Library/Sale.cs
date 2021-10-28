0fac786a738c9e9ea4f50883e1da676d8bd585f8
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2021-10-28 11:29:21 -0300

    Cambios en json

--- src/Library/Sale.cs
+++ src/Library/Sale.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ClassLibrary
 {
     /// <summary>
     /// Clase Sale.
     /// </summary>
-    public class Sale : IManagableData
+    public class Sale : IManagableData<Sale>
     {
         /// <summary>
         /// Lista de materiales de la compania.
@@ -62,6 +64,7 @@ namespace ClassLibrary
         /// <param name="DateTime"></param>
         /// <param name="Price"></param>
         /// <param name="Currency"></param>
+        [JsonConstructor]
         public Sale(DateTime DateTime, int Price, Currency Currency)
         {
             this.DateTime = DateTime;
@@ -100,6 +103,39 @@ namespace ClassLibrary
             }
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="json"></param>
+        public void LoadFromJson(string json)
+        {
+            Sale sale=JsonSerializer.Deserialize<Sale>(json);
+            this.Id=sale.Id;
+            this.DateTime=sale.DateTime;
+            this.Deleted=sale.Deleted;
+            this.Currency=sale.Currency;
+            this.Price=sale.Price;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Sale Clone()
+        {
+            Sale sale =new Sale();
+            sale.LoadFromJson(this.ConvertToJson());
+            return sale;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string ConvertToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
+    
 
 }
