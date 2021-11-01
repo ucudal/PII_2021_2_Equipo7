e29f4a972615fa95cf20e14bc44c74e31465c166
Nicolas Maisonnave <nicolas.maisonnave@correo.ucu.edu.uy>
2021-11-01 13:49:02 -0300

    Mas cambios

--- src/Library/Data/MaterialCategories/MaterialCategory.cs
+++ src/Library/Data/MaterialCategories/MaterialCategory.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace ClassLibrary
-{
+{        
     /// <summary>
     /// Esta clase representa las categorias de los materialesEmpresa.
     /// </summary>
@@ -15,55 +15,58 @@ namespace ClassLibrary
         /// Identificador de cada Categoria de Material 
         /// </summary>
         /// <value>Almacenamos un numero el cual va a identificar cada Categoria de Material.</value>
-        public int Id{get; set;}
+        public int Id { get; set; }
 
         /// <summary>
         /// Nombre de cada Categoria de Material 
         /// </summary>
         /// <value>Almacenamos un nombre de cada Categoria de Material.</value>
-        public string Name{get;set;}
+        public string Name { get; set; }
 
         /// <summary>
         /// Obtiene un valor que indica si el MaterialCategory fue eliminado
         /// </summary>
         /// <value><c>true</c> si el MaterialCategory fue eliminado, <c>false</c> en caso contrario.</value>
-        public bool Deleted{get;set;}
+        public bool Deleted { get; set; }
 
         /// <summary>
         /// Constructor de la clase
         /// </summary>
-        
-        [JsonConstructor]
-        public MaterialCategory(int id,string name)
+        public MaterialCategory(int id, string name)
         {
-            this.Id=id;
-            this.Name=name;
-            this.Deleted=false;
+            this.Id = id;
+            this.Name = name;
+            this.Deleted = false;
         }
 
         /// <summary>
         /// Constructor de la clase vacio
         /// </summary>
+        [JsonConstructor]
         public MaterialCategory()
         {
-
+            this.Id = 0;
+            this.Deleted = false;
         }
 
+        /// <inheritdoc/>
         public void LoadFromJson(string json)
         {
-            MaterialCategory category=JsonSerializer.Deserialize<MaterialCategory>(json);
-            this.Id=category.Id;
-            this.Name=category.Name;
-            this.Deleted=category.Deleted;
+            MaterialCategory category = JsonSerializer.Deserialize<MaterialCategory>(json);
+            this.Id = category.Id;
+            this.Name = category.Name;
+            this.Deleted = category.Deleted;
         }
 
+        /// <inheritdoc/>
         public MaterialCategory Clone()
         {
-            MaterialCategory category=new MaterialCategory();
+            MaterialCategory category = new MaterialCategory();
             category.LoadFromJson(this.ConvertToJson());
             return category;
         }
 
+        /// <inheritdoc/>
         public string ConvertToJson()
         {
             return JsonSerializer.Serialize(this);
