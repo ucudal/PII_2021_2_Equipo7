b4b618ed646dc6d3cae317e308230e50b2b3c2af
Nicolas Maisonnave <nicolas.maisonnave@correo.ucu.edu.uy>
2021-11-01 13:45:38 -0300

    Cambios en clases common

--- src/Library/Data/Invitations/InvitationAdmin.cs
+++ src/Library/Data/Invitations/InvitationAdmin.cs
@@ -9,13 +9,55 @@ namespace ClassLibrary
     public class InvitationAdmin: DataAdmin<Invitation>
     {
         /// <summary>
-        /// Obetener la invitacion a partir de su codigo
+        /// Obtener la invitacion a 
+        /// partir de su codigo.
         /// </summary>
-        /// <param name="pCode">Codigo de la invtacion</param>
+        /// <param name="pCode">
+        /// Codigo de la invitacion.
+        /// </param>
         /// <returns></returns>
         public Invitation GetByCode(string pCode)
         {
-            return this.Items.Find(obj=>obj.Code==pCode);
+            ReadOnlyCollection<Invitation> invites = this.Items;
+            foreach (Invitation inv in invites)
+            {
+                if (inv.Code == pCode)
+                {
+                    return inv.Clone();
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Verifica si una invitacion
+        /// pertenece al tipo sugerido.
+        /// </summary>
+        /// <param name="code">
+        /// Codigo de la invitacion a
+        /// verificar.
+        /// </param>
+        /// <param name="type">
+        /// Tipo de invitacion contra
+        /// el cual comparar.
+        /// </param>
+        /// <returns>
+        /// Valor booleano representado
+        /// si la invitacion pertenece
+        /// al tipo.
+        /// </returns>
+        public bool GetInvitationIsOfType(string code, RegistrationType type)
+        {
+            ReadOnlyCollection<Invitation> invites = this.Items;
+            foreach (Invitation inv in invites)
+            {
+                if (inv.Code == code)
+                {
+                    return inv.Type == type;
+                }
+            }
+            return false;
+
         }
     }
 }
