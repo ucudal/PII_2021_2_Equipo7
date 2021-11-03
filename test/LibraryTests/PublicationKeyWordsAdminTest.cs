47b624a474edba7ead2459567f12fd1666f8f912
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2021-11-03 09:21:25 -0300

    Cambios en publication

---
+++ test/LibraryTests/PublicationKeyWordsAdminTest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.ObjectModel;
+using ClassLibrary;
+using NUnit.Framework;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test de la clase que administra las palabras claves de las publicaciones.
+    /// </summary>
+    public class PublicationKeyWordsAdminTest
+    {
+        private PublicationKeyWordAdmin keyadmin = Singleton<PublicationKeyWordAdmin>.Instance;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Insert()
+        {
+            ReadOnlyCollection<PublicationKeyWord> items = keyadmin.Items;
+            PublicationKeyWord keyw = keyadmin.New();
+            int pInt = keyadmin.Insert(keyw);
+            Assert.AreNotEqual(0,pInt);
+        }
+
+        [Test]
+        public void NewTest()
+        {
+            PublicationKeyWord publikey = keyadmin.New();
+            Assert.IsInstanceOf(typeof(PublicationKeyWord),publikey);
+        }
+
+
+        [Test]
+        public void DeleteTest()
+        {
+            PublicationKeyWord pkey = keyadmin.New();
+            int p = keyadmin.Insert(pkey);
+            Assert.AreNotEqual(0,p);
+            int NewId =pkey.Id;
+            keyadmin.Delete(NewId);
+            Assert.IsNull(keyadmin.GetById(NewId));
+        }
+
+    }
+}
